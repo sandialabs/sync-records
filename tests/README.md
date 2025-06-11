872a5f7ac85bf2b5f59f02d58c5aded9f9bf7d72
Thien-Nam <thidinh@sandia.gov>
2025-06-10 22:54:46 -0700

    Overhaul test framework

--- tests/README.md
+++ tests/README.md
@@ -10,8 +10,29 @@ Next, make sure that a [cryptography service](https://github.com/sandialabs/sync
 
 You can now run the test by executing the following commands.
 
-`$ ./test-record.sh <path/to/journal-sdk>`
+`$ ./test.sh <path/to/journal-sdk>`
 
-`$ ./test-ledger.sh <path/to/journal-sdk>`
+## Develop
 
-`$ ./test-ontology.sh <path/to/journal-sdk>`
+This test suite supports testing of multiple journals within a discrete and deterministic framework.
+The top-level `test.sh` file leverages synchronic web primitives to convert actions and communications between multiple simulated journals into a single lisp-style expression that the SDK can evaluate.
+The `*-test.scm` within this directory contain logic for generating and executing scripted actions among multiple journals.
+To maximize flexibility, the contents of each `*-test.scm` is actually a function that accepts the following inputs:
+
+- `run-test` a function `(lambda (script) ...)` that takes a test script and runs it
+- `make-messenger` a function `(lambda (journal) ...)` that takes a journal name (symbol) and returns a function `(lambda (message) ...)` for sending messages to the journal
+- `record-src` the source code for the `record.scm` interface
+- `record-src` the source code for the `control.scm` function required by `record.scm`
+- `...` other test-specific arguments (e.g., more source code) required to run the test
+
+In general, `*-record.scm` functions can run arbitrary code to compute and format the desired test script.
+Eventually, the script that is passed into `run-test` must be a list where each item is itself a list with the following elements in order:
+
+1. `journal`: a symbol refering to the simulated journal
+2. `query`: the query to be evaluated by the simulated journal
+3. `condition`: (optional) a function or expression to check the output
+
+If the condition is a function, it must have the form `(lambda (result) ...)` to accept the returned result of the query and return an output indicating whether the script action succeeds or fails.
+If the condition is an expression, it will be compared against the result to determine success or failure using the `equals?` function.
+If the condition is omitted, then the script action is assumed to succeed.
+A test passes if all of the individual script actions succeed.
--- tests/test-record.scm
+++ tests/test-record.scm
@@ -1,71 +1,66 @@
-((test
-  (begin
-    (define (assert expr expt)
-      (let ((str (lambda (x) (byte-vector->string (expression->byte-vector x)))))
-        (let ((result (catch #t (lambda () (eval expr))
-                             (lambda args (car args)))))
-          (if (equal? result expt) #t
-              (error 'assertion (append "Expression " (str expr)
-                                        " should be " (str expt)
-                                        " but was " (str result)))))))
+(lambda (run-test make-messenger record-src control-src)
+  (let* ((pass (lambda (x) (append "pass-" (symbol->string x))))
+         (init (lambda (x) `(,x (,record-src ,(pass x) ,control-src) "Installed record interface")))
+         (form (lambda (x)
+                 `(,(car x) (*record* ,(pass (car x)) (lambda (record) ,(cadr x))) ,@(cddr x)))))
+    (run-test
+     (append
+      (map init '(journal))
+      (map form
+           `((journal ((record 'set!) '(a b) 2) #t)
+             (journal ((record 'set!) '(a c d) 4) #t)
+             (journal ((record 'set!) '(a c* d) 4) #t)
+             (journal ((record 'get) '(a c d)) '(object 4))
+             (journal ((record 'set!) '(a e f) 9) #t)
+             (journal ((record 'set!) '(a e g) 10) #t)
 
-    ;; setting
-    (assert '((record 'set!) '(a b) 2) #t)
-    (assert '((record 'set!) '(a c d) 4) #t)
-    (assert '((record 'set!) '(a c* d) 4) #t)
-    (assert '((record 'get) '(a c d)) '(object 4))
-    (assert '((record 'set!) '(a e f) 9) #t)
-    (assert '((record 'set!) '(a e g) 10) #t)
+             ;; delete
+             (journal ((record 'set!) '(a e f) #f) #t)
+             (journal ((record 'set!) '(a e) #f) #t)
 
-    ;; delete
-    (assert '((record 'set!) '(a e f) #f) #t)
-    (assert '((record 'set!) '(a e) #f) #t)
+             ;; getting
+             (journal ((record 'get) '(a)) '(directory (c c* b) #t))
+             (journal ((record 'get) '(a b)) '(object 2))
+             (journal ((record 'get) '(a c d)) '(object 4))
+             (journal ((record 'get) '(a c* d)) '(object 4))
 
-    ;; getting
-    (assert '((record 'get) '(a)) '(directory (c c* b) #t))
-    (assert '((record 'get) '(a b)) '(object 2))
-    (assert '((record 'get) '(a c d)) '(object 4))
-    (assert '((record 'get) '(a c* d)) '(object 4))
+             ;; equality
+             (journal ((record 'equal?) '(a b) '(a c)) #f)
+             (journal ((record 'equal?) '(a c) '(a c*)) #t)
 
-    ;; equality
-    (assert '((record 'equal?) '(a b) '(a c)) #f)
-    (assert '((record 'equal?) '(a c) '(a c*)) #t)
+             ;; copying
+             (journal ((record 'copy!) '(a) '(a*)) #t)
+             (journal ((record 'get) '(a* c d)) '(object 4))
 
-    ;; copying
-    (assert '((record 'copy!) '(a) '(a*)) #t)
-    (assert '((record 'get) '(a* c d)) '(object 4))
+             ;; slicing
+             (journal ((record 'get) '(a)) '(directory (c c* b) #t))
+             (journal ((record 'slice!) '(a) '(b)) #t)
+             (journal ((record 'get) '(a b)) '(object 2))
+             (journal ((record 'get) '(a)) '(directory (b) #f))
 
-    ;; slicing
-    (assert '((record 'get) '(a)) '(directory (c c* b) #t))
-    (assert '((record 'slice!) '(a) '(b)) #t)
-    (assert '((record 'get) '(a b)) '(object 2))
-    (assert '((record 'get) '(a)) '(directory (b) #f))
+             ;; serialization
+             (journal ((record 'deserialize!) '(a*) ((record 'serialize) '(a))) #t)
+             (journal ((record 'equal?) '(a* b) '(a b)) #t)
+             (journal ((record 'deserialize!) '(a*) ((record 'serialize) '(a))) #t)
+             (journal ((record 'equal?) '(a*) '(a)) #t)
 
-    ;; serialization
-    (assert '((record 'deserialize!) '(a*) ((record 'serialize) '(a))) #t)
-    (assert '((record 'equal?) '(a* b) '(a b)) #t)
-    (assert '((record 'deserialize!) '(a*) ((record 'serialize) '(a))) #t)
-    (assert '((record 'equal?) '(a*) '(a)) #t)
+             ;; pruning
+             (journal ((record 'set!) '(b a c d) 4) #t)
+             (journal ((record 'set!) '(b d d) 2) #t)
+             (journal ((record 'set!) '(b d e) 5) #t)
+             (journal ((record 'set!) '(b n c d) 1) #t)
+             (journal ((record 'copy!) '(b) '(b*)) #t)
+             (journal ((record 'prune!) '(b) '(d d) #t) #t)
+             (journal ((record 'prune!) '(b) '(d e)) #t)
+             (journal ((record 'get) '(b d)) '(directory (d) #f))
+             (journal ((record 'get) '(b d d)) '(unknown ()))
+             (journal ((record 'get) '(b n c d)) '(object 1))
 
-    ;; pruning
-    (assert '((record 'set!) '(b a c d) 4) #t)
-    (assert '((record 'set!) '(b d d) 2) #t)
-    (assert '((record 'set!) '(b d e) 5) #t)
-    (assert '((record 'set!) '(b n c d) 1) #t)
-    (assert '((record 'copy!) '(b) '(b*)) #t)
-    (assert '((record 'prune!) '(b) '(d d) #t) #t)
-    (assert '((record 'prune!) '(b) '(d e)) #t)
-    (assert '((record 'get) '(b d)) '(directory (d) #f))
-    (assert '((record 'get) '(b d d)) '(unknown ()))
-    (assert '((record 'get) '(b n c d)) '(object 1))
+             ;; equivalency
+             (journal ((record 'equal?) '(b) '(b*)) #f)
+             (journal ((record 'equivalent?) '(b) '(b*)) #t)
 
-    ;; equivalency
-    (assert '((record 'equal?) '(b) '(b*)) #f)
-    (assert '((record 'equivalent?) '(b) '(b*)) #t)
-
-    ;; merging
-    (assert '((record 'merge!) '(b) '(b*)) #t)
-    (assert '((record 'equivalent?) '(b) '(b*)) #t)
-    (assert '((record 'get) '(b* d d)) '(object 2))
-
-    'success)))
+             ;; merging
+             (journal ((record 'merge!) '(b) '(b*)) #t)
+             (journal ((record 'equivalent?) '(b) '(b*)) #t)
+             (journal ((record 'get) '(b* d d)) '(object 2))))))))
